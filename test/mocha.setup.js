eb8750844476cc6557c6e1f850479d2eae15c87a
bropat <patrick.broetto@gmail.com>
2022-02-06 21:51:01 +0100

    Fixed test and release workflow

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -4,10 +4,12 @@
 process.env.TS_NODE_IGNORE_WARNINGS = "TRUE";
 // Sets the correct tsconfig for testing
 process.env.TS_NODE_PROJECT = "tsconfig.json";
+// Make ts-node respect the "include" key in tsconfig.json
+process.env.TS_NODE_FILES = "TRUE";
 
 // Don't silently swallow unhandled rejections
 process.on("unhandledRejection", (e) => {
-    throw e;
+	throw e;
 });
 
 // enable the should interface with sinon
